ecf8f8630ab89d5530936a4c836b9c95c2fb8bc8
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-06-18 21:22:42 +0545

    remove

--- src/componet/Navbar.jsx
+++ src/componet/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Form, Link } from 'react-router-dom';
 import './navbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 import logo from '../assets/react.svg'
 
 const Navbar = () => {
@@ -15,20 +14,9 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/services">Service</Link></li>
         <li><Link to="/contact">Contact us</Link></li>
-        <li className="dropdown">
-          <Link to="/company">Company</Link>
-          <div className="dropdown-content">
-            <Link to="#">About Us</Link>
-            <Link to="#">Our Team</Link>
-            <Link to="#">Careers</Link>
-          </div>
-        </li>
+        
       </ul>
-      <div className="navbar-icons">
-        <a href="https://github.com/Mandip69/React-js-"><FontAwesomeIcon icon={faGithub} /></a>
-        <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
-      </div>
+      
     </nav>
   );
 };
